e0282bcf8374940cc3fc56cf63cb6f1c2e9f2749
Bryan Nip <bryannip@buffalo.edu>
2020-01-18 18:00:00 -0500

    Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -22,6 +21,12 @@ class App extends Component {
         }
       ]
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
